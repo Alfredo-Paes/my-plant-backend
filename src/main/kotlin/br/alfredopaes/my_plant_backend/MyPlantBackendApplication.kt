386a409ad2f3593c32494c4e5cc20b4f6528abba
Alfredo-Paes <alfredo.alfpaes@gmail.com>
2024-05-29 02:32:11 -0300

    Created Project

---
+++ src/main/kotlin/br/alfredopaes/my_plant_backend/MyPlantBackendApplication.kt
@@ -0,0 +1,11 @@
+package br.alfredopaes.my_plant_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyPlantBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyPlantBackendApplication>(*args)
+}
---
+++ src/test/kotlin/br/alfredopaes/my_plant_backend/MyPlantBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.alfredopaes.my_plant_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyPlantBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
